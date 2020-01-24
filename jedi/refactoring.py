7467cae68b8babf268f9da07854c448f427db1c9
Josh Bax <joshbax189@gmail.com>
2020-01-24 09:37:52 -0800

    Prelim version of refactoring.inline

--- jedi/refactoring.py
+++ jedi/refactoring.py
@@ -159,39 +159,91 @@ def extract(script, new_name):
     return Refactoring(dct)
 
 
-def inline(script):
+def inline(script, line=None, column=None):
     """
+    Replace a variable with its definition
     :type script: api.Script
     """
-    new_lines = split_lines(python_bytes_to_unicode(script.source))
+    new_lines = split_lines(python_bytes_to_unicode(script._code))
 
     dct = {}
 
-    definitions = script.goto()
+    definitions = script.goto(line, column)
     assert len(definitions) == 1
-    stmt = definitions[0]._definition
-    references = script.get_references()
-    inlines = [r for r in references
-               if not stmt.start_pos <= (r.line, r.column) <= stmt.end_pos]
-    inlines = sorted(inlines, key=lambda x: (x.module_path, x.line, x.column),
-                     reverse=True)
-    expression_list = stmt.expression_list()
-    # don't allow multi-line refactorings for now.
-    assert stmt.start_pos[0] == stmt.end_pos[0]
-    index = stmt.start_pos[0] - 1
 
+    stmt = definitions[0]
+    assert stmt.type == 'statement'
+    # TODO disallow keywords, methods here
+
+    # a list of definitions
+    references = script.get_references(line, column)
+
+    # TODO
+    # don't allow multi-line refactorings for now.
+    # assert stmt.start_pos[0] == stmt.end_pos[0] # TODO
+
+    # Examples include, multiline lists, dicts, statement continuations or parens.
+
+    # TODO factor this into a method
+    # search forward until next definition, checking for reuses
+    refs2 = []
+    newDefLine = 0
+    for r in references:
+        if (r.line, r.column) == (stmt.line, stmt.column):
+            continue
+
+        if r.is_definition():
+            newDefLine = r.line
+            continue
+
+        if newDefLine > 0:
+            if r.line == newDefLine:
+                refs2.append(r)
+            else:
+                break
+        else:
+            refs2.append(r)
+
+    # inlines = [r for r in references
+    #            if (r.line, r.column) != (stmt.line, stmt.column)]
+    inlines = sorted(refs2, key=lambda x: (x.module_path, x.line, x.column),
+                     reverse=True)
+
+    index = stmt.line - 1
     line = new_lines[index]
-    replace_str = line[expression_list[0].start_pos[1]:stmt.end_pos[1] + 1]
-    replace_str = replace_str.strip()
-    # tuples need parentheses
-    if expression_list and expression_list[0].type == 'TODO':
-        arr = expression_list[0]
-        if replace_str[0] not in ['(', '[', '{'] and len(arr) > 1:
+
+    # the parent expression of the form "x = expr"
+    assign_stmt = stmt._name.tree_name.parent
+
+    if len(stmt._name.assignment_indexes()) > 0:
+        # the same as parso.tree.search_ancestor()
+        while assign_stmt.type != 'expr_stmt':
+            assign_stmt = assign_stmt.parent
+
+        expr_tree = assign_stmt.get_rhs()
+        replace_str = expr_tree.get_code(include_prefix=False)
+    else:
+        expr_tree = assign_stmt.children[2]
+        replace_str = expr_tree.get_code(include_prefix=False)
+
+    # tuples and lambdas need parentheses
+    if (expr_tree.type == 'testlist_star_expr' or
+        expr_tree.type == 'lambdef' or
+        expr_tree.type == 'lambdef_nocond'):
+        if replace_str[0] not in ['(', '[', '{']:
             replace_str = '(%s)' % replace_str
 
-    # if it's the only assignment, remove the statement
-    if len(stmt.get_defined_names()) == 1:
-        line = line[:stmt.start_pos[1]] + line[stmt.end_pos[1]:]
+    # TODO eliminate del, nonlocal
+
+    # if len(stmt.get_defined_names()) == 1:
+    #     line = line[:stmt.start_pos[1]] + line[stmt.end_pos[1]:]
+
+    line_prefix = line[:assign_stmt.start_pos[1]]
+    line_suffix = line[assign_stmt.end_pos[1]:].strip()
+
+    if line_suffix and line_suffix[0] == ';':
+        line_suffix = line_suffix[1:].strip()
+    line = line_prefix + line_suffix
 
     dct = _rename(inlines, replace_str)
     # remove the empty line
